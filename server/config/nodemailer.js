52a1a07d194304e8dcf058624804b48e47949381
eaglefluke21 <yadavankit7fc4@gmail.com>
2024-06-11 18:06:45 +0530

    tracking on other pc

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,18 @@
 import nodemailer from nodemailer;
+import dotenv from 'dotenv';
 
+
+dotenv.config();
+
+const USER = process.env.EMAIL;
+const PASS = process.env.EMAILPASSWORD;
 
 //object to send mail to users
 export const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: '',
-        pass: '',
+        user: 'USER',
+        pass: 'PASS',
         
     },
 });
---
+++ server/utils/email.js
@@ -0,0 +1,29 @@
+import { transporter } from "../config/nodemailer";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const EMAIL = process.env.EMAIL;
+const backendurl = 'http://localhost:3000';
+
+
+export const sendRestEmail = () => {
+    const resetUrl = `${backendurl}/users/reset-password/${token}`;
+    const mailOptions = {
+        from: EMAIL,
+        to: email,
+        subject: 'Password Reset',
+        text: `You requestd a password reset. Click here to reset your password. ${resetUrl}`,
+
+    };
+
+transporter.sendMail(mailOptions, (error,info) => {
+    if(error){
+        console.error('Error sending email:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
+
+
+};
